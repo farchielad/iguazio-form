b3d45f88e826822e00c69ec749a6245c6e562cd3
eladfarchi <elad.fa@solaredge.com>
2024-02-08 18:53:02 +0200

    final version 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from './components/form/form.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
